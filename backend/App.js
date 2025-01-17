2a539d8bf8a3dc23560b8cb12b50a94aebbc2077
Nitika-singh-4 <Nitikasingh262@gmail.com>
2025-01-17 11:03:48 +0530

    updated the changes

--- backend/App.js
+++ backend/App.js
@@ -3,11 +3,14 @@ const connectDatabase=require('./db/Database')
 const ErrorHandler = require('./utils/ErrorHandler')
 const cookieParser=require('cookie-parser');
 const bodyParser=require('body-parser')
+const cors=require('cors')
 const app=express()
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 app.use("/",express.static("uploads"))
-app.use(bodyParser.urlencoded({extended:true, limit:'50md'}))
+app.use(bodyParser.urlencoded({extended:true, limit:'50md'}));
+app.use(cors())
 //config
 if(process.env.NODE_ENV !=="PRODUCTION"){
     require('dotenv').config({
---
+++ backend/utils/jwtToken.js
@@ -0,0 +1,18 @@
+const sendToken=(user,statusCode,res)=>{
+    const token=user.getJwtToken();
+
+    const option={
+        expires: new Date(
+            Date.now()+90*5215128*1000
+        ),
+        httpOnly: true,
+    }
+
+    res.status(statusCode).cookie("token",token, option).json({
+        success:true,
+        token,
+        user
+    })
+}
+
+module.exports=sendToken;
