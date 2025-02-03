2c8e27032ef7d479c060a95564bcc1a8217019b2
Nitika-singh-4 <Nitikasingh262@gmail.com>
2025-02-03 14:08:26 +0530

    milestone12 completed

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -2,5 +2,6 @@ import LoginPage from './pages/Login'
 import SignupPage from "./pages/Signup";
 import Home from './pages/Home';
 import CreateProduct from './pages/createProduct'; 
+import MyProducts from './pages/myProduct';
 
-export {LoginPage,SignupPage,Home,CreateProduct};
+export {LoginPage,SignupPage,Home,CreateProduct, MyProducts};
